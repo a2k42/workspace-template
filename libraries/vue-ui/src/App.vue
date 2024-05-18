6e48a13eaa8a6d34c8c1a557bdf2abf0b7beaff1
a2k42 <1123245+a2k42@users.noreply.github.com>
2024-05-18 08:41:46 +0100

    Added test component

--- libraries/vue-ui/src/App.vue
+++ libraries/vue-ui/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-
+import VButton from "@/components/VButton.vue"
 </script>
 
 <template>
 <header>
     <h1>Vue UI</h1>
+    <v-button>Vue Button</v-button>
 </header>
 </template>
 
---
+++ libraries/vue-ui/src/components/VButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <button>
+        <slot />
+    </button>
+</template>
+
+<style scoped>
+button {
+    background: linear-gradient(70deg, #999, #CCC);
+    cursor: pointer;
+    padding: 1em;
+}
+</style>
